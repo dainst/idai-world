f2c485c94ee946a42861f8a547fcda8de08ea0a9
Mr.T <markus.troschinski@martian.company>
2019-05-09 21:15:41 +0200

    feature: ticket 0000994 - extended slider options

--- src/app/components/slider-image/slider-image.component.ts
+++ src/app/components/slider-image/slider-image.component.ts
@@ -1,6 +1,11 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { SliderBase } from 'src/app/core/components/slider/slider.base';
 
+const ALIGNMENTS = {
+  center: 'center',
+  left: 'left',
+  right: 'right'
+};
 @Component({
   selector: 'dai-slider-image .section-archeological-images',
   templateUrl: './slider-image.component.html',
@@ -11,24 +16,38 @@ export class SliderImageComponent extends SliderBase {
   public images: any[] = [];
 
   public slideHeight: string;
+  public useFullWidth = true;
+  public align = ALIGNMENTS.center; // 'left', 'right
 
   sliderConfig = {
     fade: true,
-    speed: 5000,
+    speed: 2000,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 0
+    autoplaySpeed: 5000
   };
 
   onSetConfig = (
-    config = { title: '', description: '', content: [], height: '' }
+    config = {
+      title: '',
+      description: '',
+      content: [],
+      height: '',
+      fullWidth: true,
+      align: ''
+    }
   ) => {
     const {
       title = '',
       description = '',
       content = [],
-      height = '400px'
+      height = '400px',
+      fullWidth = true,
+      align
     } = config;
+
+    this.useFullWidth = fullWidth;
+    this.align = align in ALIGNMENTS ? align : ALIGNMENTS.center;
 
     const defaultTarget = 'route://.';
 
