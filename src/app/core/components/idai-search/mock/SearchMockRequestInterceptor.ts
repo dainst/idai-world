619b8b6e01c57676b0aa0efe3079cbd5447c1d94
Adrian Rudnik <adrian@klonmaschine.de>
2021-04-12 11:33:09 +0200

    feat (search): Disabled search mock request interceptor

--- src/app/core/components/idai-search/mock/SearchMockRequestInterceptor.ts
+++ src/app/core/components/idai-search/mock/SearchMockRequestInterceptor.ts
@@ -1,26 +1,27 @@
-import { Injectable, Injector } from "@angular/core";
+import {Injectable, Injector} from '@angular/core';
 import {
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
   HttpResponse,
-} from "@angular/common/http";
-import { Observable, of } from "rxjs";
-import { SearchResult } from "src/app/generated/search";
-import { makeSearchResult } from "./search-mock";
+} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {SearchResult} from 'src/app/generated/search';
+import {makeSearchResult} from './search-mock';
 
 @Injectable()
 export class SearchMockRequestInterceptor implements HttpInterceptor {
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector) {
+  }
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log("Intercepted http call:", request.url);
+    console.log('Intercepted http call:', request.url);
 
-    if (request.url.endsWith("search") && request.method == "GET") {
+    if (request.url.endsWith('localsearch') && request.method === 'GET') {
       return new Observable((observer) => {
         observer.next(
           new HttpResponse<SearchResult>({
